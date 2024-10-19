7e0b2ffdd1e7bbf2d145c3b0c31bf515191a6701
denner <denneralveslage@gmail.com>
2024-10-18 22:34:26 -0300

    primeiro commit, inicialização do projeto

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import { Button } from '@mui/material'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      
+      <h1>Bingo Limeirense</h1>
+      <Button variant='contained'>Iniciar</Button>
+    </>
+  )
+}
+
+export default App
